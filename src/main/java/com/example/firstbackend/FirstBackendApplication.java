0bd1f3c9db175a93d5903da1118936c8bbb0259d
Morelle-Olivia <hotoumorelle@gmail.com>
2022-03-24 09:03:16 +0100

    New project

---
+++ src/main/java/com/example/firstbackend/FirstBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/firstbackend/FirstBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
